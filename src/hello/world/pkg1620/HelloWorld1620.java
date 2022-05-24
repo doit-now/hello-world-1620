5804869cece9fffe709746dc3f6af8f5a198a827
doit-now <hoang.nguyenthe@gmail.com>
2022-05-24 11:29:26 +0700

    1st lan dau lam chuyen ay - upload to github

---
+++ src/hello/world/pkg1620/HelloWorld1620.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world.pkg1620;
+
+/**
+ *
+ * @author giao.lang
+ */
+public class HelloWorld1620 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to SE1620. GitHub is so funny!!!");
+    }
+    
+}
